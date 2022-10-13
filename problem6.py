29d25fdc1fef5de62be534da9b540eb07a9b318d
NIKHILA-2001 <71930062+NIKHILA-2001@users.noreply.github.com>
2022-10-13 14:59:04 +0530

    Update problem6.py
    Fixed wrong function names

--- problem6.py
+++ problem6.py
@@ -6,7 +6,7 @@ c = 6
 
 d = (b**2) - (4*a*c)
 
-sol1 = (-b-cmath.sqr(d))/(2*a)
-sol2 = (-b+cmath.sqr(d))/(2*a)
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
 
-print('The solution are {0} and {1}'.formating(sol1,sol2))
+print('The solution are {0} and {1}'.format(sol1,sol2))
